1b87532387ef799f76565112aeaa97a6d86dd17c
maimuna omar <maymunaabdi07@gmail.com>
2023-05-20 12:00:21 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="About">
+      <h2>About ME</h2> 
+      <p>"I'm a curious and passionate individual with a background in technology and a love for creativity. I enjoy designing user experiences and solving complex problems. In my free time, I explore nature, read books, and experiment with cooking. I believe in continuous growth and embrace opportunities for personal and professional development. Let's create something extraordinary together!"</p>
+      <p>I Made This</p>
+      <img src={image} alt="i made this" />
+    </div>
+  );
 }
 
 export default About;
+
